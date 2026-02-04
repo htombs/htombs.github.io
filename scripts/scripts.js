abd0d316cc40c42c334cb72c05789628fe5cf017
Harry Tombs <h.tombs91@gmail.com>
2026-02-04 11:37:24 +0000

    condensed 5 functions into 1 to reduce redundant code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,37 +2,46 @@ function toggleMobileMenu(menu) {
     menu.classList.toggle('open');
 }
 
-function showimg0(){
-    let img0 = document.getElementById("profile_image0")
-    if (!img0.src.match("./media/profile_photo.jpg")) {
-        img0.src = "./media/profile_photo.jpg"
+function showImage(imageIndex) {
+    const imagePath = `./media/profile_photo${imageIndex === 0 ? '' : imageIndex}.jpg`;
+    const img = document.getElementById("profile_image0");
+
+    if (!img.src.match(imagePath)) {
+        img.src = imagePath;
     }
 }
 
-function showimg1(){
-    let img1 = document.getElementById("profile_image0")
-    if (!img1.src.match("./media/profile_photo1.jpg")) {
-        img1.src = "./media/profile_photo1.jpg"
-    }
-}
+// function showimg0(){
+//     let img0 = document.getElementById("profile_image0")
+//     if (!img0.src.match("./media/profile_photo.jpg")) {
+//         img0.src = "./media/profile_photo.jpg"
+//     }
+// }
 
-function showimg2(){
-    let img2 = document.getElementById("profile_image0")
-    if (!img2.src.match("./media/profile_photo2.jpg")) {
-        img2.src = "./media/profile_photo2.jpg"
-    }
-}
+// function showimg1(){
+//     let img1 = document.getElementById("profile_image0")
+//     if (!img1.src.match("./media/profile_photo1.jpg")) {
+//         img1.src = "./media/profile_photo1.jpg"
+//     }
+// }
 
-function showimg3(){    
-    let img3 = document.getElementById("profile_image0")
-    if (!img3.src.match("./media/profile_photo3.jpg")) {
-        img3.src = "./media/profile_photo3.jpg"
-    }
-}
+// function showimg2(){
+//     let img2 = document.getElementById("profile_image0")
+//     if (!img2.src.match("./media/profile_photo2.jpg")) {
+//         img2.src = "./media/profile_photo2.jpg"
+//     }
+// }
 
-function showimg4(){
-    let img4 = document.getElementById("profile_image0")
-    if (!img4.src.match("./media/profile_photo4.jpg")) {
-        img4.src = "./media/profile_photo4.jpg"
-    }
-}
+// function showimg3(){    
+//     let img3 = document.getElementById("profile_image0")
+//     if (!img3.src.match("./media/profile_photo3.jpg")) {
+//         img3.src = "./media/profile_photo3.jpg"
+//     }
+// }
+
+// function showimg4(){
+//     let img4 = document.getElementById("profile_image0")
+//     if (!img4.src.match("./media/profile_photo4.jpg")) {
+//         img4.src = "./media/profile_photo4.jpg"
+//     }
+// }
